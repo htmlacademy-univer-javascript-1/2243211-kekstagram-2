1c5e9d7fca9fa3abc1c534cb64f215938d2073f8
Eugene <zeazagaynov2002@mail.ru>
2023-04-13 16:53:04 +0500

    added a function that returns a random number in mine.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 const checkLength = (str, maxLength) => str.lenght <= maxLength;
+
+function getRandomInt (min, max) {
+if (min < 0 || max < 0) {
+    throw new RangeError("Числа в диапазоне должны быть неотрицательными.");
+}
+
+if (min > max) {
+[min, max] = [max, min];
+}
+
+if (min === max) {
+    return max;
+}
+
+min = Math.ceil(min);
+max = Math.floor(max);
+
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}; 
